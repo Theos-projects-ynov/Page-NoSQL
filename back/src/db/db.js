9203c8a51acbf59db3f603fff29d93e44ee86428
DariusDreem <reungoat.nathan@gmail.com>
2025-02-18 08:11:20 +0100

    css front

--- back/src/db/db.js
+++ back/src/db/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connexionDB = () => {
-    mongoose.connect('mongodb://localhost:27017/google_forms')
+    mongoose.connect('mongodb+srv://stoffelbachtheos:HyTHPNSavOKOALq6@cluster0.t0c8u.mongodb.net/')
         .then(() => {
             console.log('\x1b[32m\x1b[1m%s\x1b[0m', 'Connection to MongoDB successful');
         })
